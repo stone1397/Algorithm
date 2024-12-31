d17078aa9b198e60c4104e2e17edcc8026bd6542
stone1397 <157870045+stone1397@users.noreply.github.com>
2024-12-31 10:08:43 +0900

    [level 1] Title: 직사각형 별찍기, Time: 3.64 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    string stars(a, '*');
+    for (int i = 0; i < b; i++){
+        cout << stars << endl;
+    }
+    
+    return 0;
+}
